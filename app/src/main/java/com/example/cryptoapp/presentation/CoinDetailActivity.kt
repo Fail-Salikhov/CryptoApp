dfeececf95ca3ab7fd7aad9cf5a95a1e8438fb84
Fail Salikhov <sff1313@yandex.ru>
2022-12-20 17:36:19 +0400

    add fragmen for 2n screen

--- app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -8,11 +8,11 @@ import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
+import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: CoinViewModel
 
     private val binding by lazy { ActivityCoinDetailBinding.inflate(layoutInflater) }
 
@@ -24,21 +24,12 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_FROM_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this, Observer { it ->
-           with(binding){
-               tvPrice.text = it.price
-               tvMinPrice.text = it.lowday
-               tvMaxPrice.text = it.highday
-               tvLastMarket.text = it.lastmarket
-               tvLastUpdate.text = it.lastupdate
-               tvFromSymbol.text = it.fromsymbol
-               tvToSymbol.text = it.tosymbol
-               Picasso.get().load(it.imageurl).into(ivLogoCoin)
-               Log.d("testdetail", it.toString())
-           }
-
-        })
+        if (savedInstanceState == null ){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container_coin_detail, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
+        }
 
     }
 
