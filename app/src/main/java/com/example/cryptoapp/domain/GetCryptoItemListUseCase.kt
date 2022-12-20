c2777cf93db16defbcd889f32e7ce985b69cfb64
Fail Salikhov <sff1313@yandex.ru>
2022-12-20 13:19:24 +0400

    refactor app 2

--- app/src/main/java/com/example/cryptoapp/domain/GetCryptoItemListUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/GetCryptoItemListUseCase.kt
@@ -4,11 +4,6 @@ import androidx.lifecycle.LiveData
 
 class GetCryptoItemListUseCase(private val cryptoRepository: CryptoRepository) {
 
-<<<<<<< HEAD
     operator fun invoke() = cryptoRepository.getCryptoItemListUseCase()
-=======
-    fun getCryptoItemListUseCase () : LiveData<List<CryptoItem>> {
-        return cryptoRepository.getCryptoItemListUseCase()
-    }
->>>>>>> origin/master
+
 }
--- app/src/main/java/com/example/cryptoapp/domain/GetCryptoItemUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/GetCryptoItemUseCase.kt
@@ -1,16 +1,9 @@
 package com.example.cryptoapp.domain
 
-<<<<<<< HEAD
-import androidx.lifecycle.LiveData
+
 
 class GetCryptoItemUseCase(private val cryptoRepository: CryptoRepository) {
 
     operator fun invoke (fromSymbol: String) = cryptoRepository.getCryptoItem(fromSymbol)
-=======
-class GetCryptoItemUseCase(private val cryptoRepository: CryptoRepository) {
 
-    fun getCryptoItem (cryptoItemId: Int) : CryptoItem {
-        return cryptoRepository.getCryptoItem(cryptoItemId)
-    }
->>>>>>> origin/master
 }
