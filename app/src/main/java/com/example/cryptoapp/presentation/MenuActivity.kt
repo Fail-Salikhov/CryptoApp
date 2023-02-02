2780ce60652c78b2c4ca4a10b80b36ee5ebbe4ec
Fail Salikhov <sff1313@yandex.ru>
2023-02-02 22:49:55 +0400

    Create drawer menu CoinList сделал основным при старте

--- app/src/main/java/com/example/cryptoapp/presentation/MenuActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cryptoapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cryptoapp.databinding.ActivityMenuBinding
@@ -14,5 +15,7 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val intent = Intent(this, CoinPriceListActivity::class.java)
+        startActivity(intent)
     }
 }
